a53fbee37bfbc0067ab267ed6a825dda1272246c
arthis <artissae@hotmail.com>
2012-04-11 16:38:28 +0300

    process.on('SIGINT', function () ... was giving Error: No such module

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var config = require ('./config').values
-
+var tty = require('tty');
 var app = require ('./app').getApp(config)
+
+process.stdin.resume();
+tty.setRawMode(true);
 
 var port = parseInt(process.argv[2], 10) || 3001
 app.listen(port);
@@ -11,9 +14,12 @@ race.createRace(app);
 
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-process.on('SIGINT', function () {
-	app.close();
+
+process.stdin.on('keypress', function(char, key) {
+  if (key && key.ctrl && key.name == 'c') {
+    app.close();
 	console.log();
 	console.log('Shutting down server..');
 	process.exit(0);
+  }
 });
